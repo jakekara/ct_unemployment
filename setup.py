ca99276a650a6a141d9fd7b40530f1f0c7a1d6ff
Jake Kara <jake@jakekara.com>
2017-12-22 13:00:54 -0500

    Added github URL to setup file

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
     name="ct-dol",
-    version="0.1",
+    version="0.1.1",
     description="A tool for pulling down ct unemployment as a dataframe",
+    url="https://github.com/jakekara/ct_unemployment",
     author="Jake Kara",
     author_email="jake@jakekara.com",
     license="GPL-3",
     install_requires=["pandas"],
-    packages=["ct_unemployment"]
+    packages=["ct_unemployment"],
+    
     
 )
     
